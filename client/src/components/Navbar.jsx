6a3c4598c6daaea11bc3737af24c454dd1839ec9
Lubna Febin <lubnafebin13@gmail.com>
2025-10-12 22:04:32 +0530

    added navbar component

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32">
+      <img src="/favicon.svg" alt="" />
+    </div>
+  );
+};
