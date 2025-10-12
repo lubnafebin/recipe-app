07b4c299e8110bc613ed3f0bf8f727b2fbc6d26c
Lubna Febin <lubnafebin13@gmail.com>
2025-10-12 22:04:09 +0530

    added home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,11 @@
-import React from "react";
-
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
 export const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 };
