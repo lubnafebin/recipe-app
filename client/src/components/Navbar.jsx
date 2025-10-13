5738ee3a258a6557686033df76fa6831f36c6e47
Lubna Febin <lubnafebin13@gmail.com>
2025-10-13 10:54:40 +0530

    added react icons package,updated navbar added logo and login button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,23 @@
-import React from "react";
-
+import { useNavigate } from "react-router-dom";
+import logo from "../assets/logo.png";
+import { CiLogin } from "react-icons/ci";
 export const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32">
-      <img src="/favicon.svg" alt="" />
+      <img
+        onClick={() => navigate("/")}
+        src={logo}
+        alt=""
+        className="w-32 sm:44 cursor-pointer"
+      />
+      <button
+        onClick={() => navigate("/admin")}
+        className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-6 py-2"
+      >
+        <CiLogin />
+        Login
+      </button>
     </div>
   );
 };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,9 @@
+import { Navbar } from "../components/Navbar";
+
 export const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 };
