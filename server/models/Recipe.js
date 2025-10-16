4252b0b9bb8d451f18505dc9a75dedb9f3dd3309
Lubna Febin <lubnafebin13@gmail.com>
2025-10-16 21:26:27 +0530

    added recipe model

---
+++ server/models/Recipe.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const RecipeSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: [
+        "Quick & Easy",
+        "Healthy Choices",
+        "Traditional Flavors",
+        "Sweet Treats",
+      ],
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    ingredients: {
+      type: [String],
+      default: [],
+    },
+    instructions: {
+      type: [String],
+      default: [],
+    },
+    isPublished: { type: Boolean, required: true },
+  },
+  { timestamps: true }
+);
+
+const Recipe = mongoose.model("recipe", RecipeSchema);
+export default Recipe;
