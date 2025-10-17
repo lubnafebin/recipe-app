1a362205edfe5c89b89725bf14a4e594f8703541
Lubna Febin <lubnafebin13@gmail.com>
2025-10-17 21:11:17 +0530

    added admin controller

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,4 +1,6 @@
 import jwt from "jsonwebtoken";
+import Recipe from "../models/Recipe.js";
+import Comment from "../models/Comment.js";
 export const adminLogin = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -13,6 +15,66 @@ export const adminLogin = async (req, res) => {
     }
     const token = jwt.sign({ email }, process.env.JWT_SECRET);
     res.json({ success: true, token });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const getAllRecipesAdmin = async (req, res) => {
+  try {
+    const recipes = await Recipe.find({}).sort({ createdAt: -1 });
+    res.json({ success: true, recipes });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const getAllComments = async (req, res) => {
+  try {
+    const comments = await Comment.find({})
+      .populate("recipe")
+      .sort({ createdAt: -1 });
+    res.json({ success: true, comments });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const getDashboard = async (req, res) => {
+  try {
+    const recentRecipes = await Recipe.find({})
+      .sort({ createdAt: -1 })
+      .limit(5);
+    const recipes = await Recipe.countDocuments();
+    const comments = await Comment.countDocuments();
+    const drafts = await Recipe.countDocuments({ isPublished: false });
+    const dashboardData = {
+      recentRecipes,
+      recipes,
+      comments,
+      drafts,
+    };
+    res.json({ success: true, dashboardData });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const deleteCommentById = async (req, res) => {
+  try {
+    const { id } = req.body;
+    await Comment.findByIdAndDelete(id);
+    res.json({ success: true, message: "Comment deleted successfully" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const approveCommentById = async (req, res) => {
+  try {
+    const { id } = req.body;
+    await Comment.findByIdAndUpdate(id, { isApproved: true });
+    res.json({ success: true, message: "Comment approved successfully" });
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
