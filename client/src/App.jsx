9a8ed25a5cb255876df67afb9d8ea0c55e66f0ce
Lubna Febin <lubnafebin13@gmail.com>
2025-10-19 22:07:40 +0530

    added login component and context for auth management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,15 +9,17 @@ import { Comments } from "./pages/admin/Comments";
 import { Login } from "./components/admin/Login";
 import "quill/dist/quill.snow.css";
 import { Toaster } from "react-hot-toast";
+import { useAppContext } from "./context/AppContext";
 export const App = () => {
+  const { token } = useAppContext();
+
   return (
     <div>
       <Toaster />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recipe/:id" element={<Recipe />} />
-        <Route path="/admin" element={<Layout />}>
-          {/* <Login /> */}
+        <Route path="/admin" element={token ? <Layout /> : <Login />}>
           <Route index element={<Dashboard />} />
           <Route path="add-recipe" element={<AddRecipe />} />
           <Route path="list-recipe" element={<ListRecipe />} />
