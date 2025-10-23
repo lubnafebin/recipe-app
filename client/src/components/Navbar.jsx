ff525e51893d6a62e10f62261f44cc12928ade51
Lubna Febin <lubnafebin13@gmail.com>
2025-10-23 10:47:04 +0530

    feat: update Navbar component styles for improved layout and responsiveness

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,15 +5,15 @@ import { CiCirclePlus } from "react-icons/ci";
 export const Navbar = () => {
   const { navigate, token } = useAppContext();
   return (
-    <div className="flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32">
+    <div className="flex justify-between items-center py-5 mx-4 sm:mx-16 xl:mx-28">
       <img
         onClick={() => navigate("/")}
         src={logo}
         alt=""
-        className="w-32 sm:44 cursor-pointer"
+        className="w-24 sm:w-32 cursor-pointer"
       />
-      <div className="flex items-center gap-10">
-        <div className="flex items-center gap-1 text-primary">
+      <div className="flex items-center gap-4 sm:gap-10">
+        <div className="flex items-center gap-1 text-primary text-sm sm:text-base">
           <CiCirclePlus />
           <span
             className="cursor-pointer"
@@ -24,7 +24,8 @@ export const Navbar = () => {
         </div>
         <button
           onClick={() => navigate("/admin")}
-          className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-6 py-2"
+          className="flex items-center gap-1 rounded-full text-xs sm:text-sm cursor-pointer bg-primary 
+          text-white px-3 py-1.5 sm:px-6 sm:py-2"
         >
           <CiLogin />
           {token ? "Dashboard" : "Login"}
