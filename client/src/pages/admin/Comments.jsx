376469e28093db34d54f405815c8f376113adebe
Lubna Febin <lubnafebin13@gmail.com>
2025-10-23 10:56:50 +0530

    style: improve layout and spacing in Comments component for better readability

--- client/src/pages/admin/Comments.jsx
+++ client/src/pages/admin/Comments.jsx
@@ -20,13 +20,13 @@ export const Comments = () => {
     fetchComments();
   }, []);
   return (
-    <div className="flex-1 pt-5 px-5 sm:pl-16 sm:pt-12 bg-blue-50/50">
-      <div className="flex justify-between items-center max-w-3xl">
+    <div className="flex-1 pt-5 px-3 sm:px-5 sm:pl-16 sm:pt-12 bg-blue-50/50">
+      <div className="flex flex-col sm:flex-row justify-between items-center max-w-3xl gap-4 sm:gap-0">
         <h1>Comments</h1>
-        <div className="flex gap-4">
+        <div className="flex gap-2 sm:gap-4">
           <button
             onClick={() => setFilter("Approved")}
-            className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs ${
+            className={`shadow-custom-sm border rounded-full px-3 sm:px-4 py-1 cursor-pointer text-[10px] sm:text-xs ${
               filter === "Approved" ? "text-primary" : "text-gray-700"
             }`}
           >
@@ -34,7 +34,7 @@ export const Comments = () => {
           </button>
           <button
             onClick={() => setFilter("Not Approved")}
-            className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs ${
+            className={`shadow-custom-sm border rounded-full px-3 sm:px-4 py-1 cursor-pointer text-[10px] sm:text-xs ${
               filter === "Not Approved" ? "text-primary" : "text-gray-700"
             }`}
           >
@@ -43,20 +43,17 @@ export const Comments = () => {
         </div>
       </div>
 
-      <div className="relative h-4/5 max-w-3xl overflow-x-auto mt-4 bg-white shadow rounded-lg  scrollbar-hide">
-        <table className="w-full text-sm text-gray-500">
-          <thead className="text-xs text-gray-700 text-left uppercase">
+      <div className="relative h-4/5 max-w-3xl overflow-x-auto mt-4 bg-white shadow rounded-lg scrollbar-hide">
+        <table className="w-full text-[11px] sm:text-sm text-gray-500">
+          <thead className="text-[10px] sm:text-xs text-gray-700 text-left uppercase">
             <tr>
-              <th scope="col" className="px-6 py-3">
-                {" "}
-                Recipe Title&Comments{" "}
+              <th scope="col" className="px-2 sm:px-6 py-3">
+                Recipe Title&Comments
               </th>
-              <th scope="col" className="px-6 py-3 mx-sm:hidden">
-                {" "}
-                Date{" "}
+              <th scope="col" className="hidden sm:table-cell px-6 py-3">
+                Date
               </th>
-              <th scope="col" className="px-6 py-3">
-                {" "}
+              <th scope="col" className="px-2 sm:px-6 py-3">
                 Action
               </th>
             </tr>
