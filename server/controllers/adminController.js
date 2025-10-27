c1ff7dc0fed51243dd62f7a2569572a1e0e320c2
Lubna Febin <lubnafebin13@gmail.com>
2025-10-27 19:58:36 +0530

    feat: implement admin registration and login functionality with user validation

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,20 +1,73 @@
 import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 import Recipe from "../models/Recipe.js";
 import Comment from "../models/Comment.js";
+import User from "../models/User.js";
+
+export const adminRegister = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.json({ success: false, message: "Missing Details" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    return res.json({
+      success: true,
+      token,
+      user: { email: user.email, name: user.name },
+    });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
 export const adminLogin = async (req, res) => {
   try {
     const { email, password } = req.body;
-    if (
-      email !== process.env.ADMIN_EMAIL ||
-      password !== process.env.ADMIN_PASSWORD
-    ) {
+    if (!email || !password)
       return res.json({
         success: false,
-        message: "Invalid Credentials",
+        message: "Email and Password are required",
       });
-    }
-    const token = jwt.sign({ email }, process.env.JWT_SECRET);
-    res.json({ success: true, token });
+
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.json({
+        success: false,
+        message: "Invalid username or password",
+      });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.json({
+        success: false,
+        message: "Invalid username or password",
+      });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    return res.json({
+      success: true,
+      token,
+      message: "Login Successfully",
+      user: { email: user.email, name: user.name },
+    });
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
