b4e3ac7adae3417bac75535c78daa21921399688
Lubna Febin <lubnafebin13@gmail.com>
2025-10-28 21:31:23 +0530

    updated handlesubmit for register form

--- client/src/components/admin/Login.jsx
+++ client/src/components/admin/Login.jsx
@@ -13,10 +13,11 @@ export const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(`/api/admin/${state}`, {
+        name,
         email,
         password,
       });
-
+      
       if (data.success) {
         setToken(data.token);
         localStorage.setItem("token", data.token);
