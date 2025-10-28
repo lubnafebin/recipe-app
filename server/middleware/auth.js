d4d4164d80a8f0054de513c6bc24d325059b1995
Lubna Febin <lubnafebin13@gmail.com>
2025-10-28 21:34:37 +0530

    updated auth by adding user to req user

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,7 +3,8 @@ const auth = (req, res, next) => {
   const token = req.headers.authorization;
 
   try {
-    jwt.verify(token, process.env.JWT_SECRET);
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
     next();
   } catch (error) {
     res.json({ success: false, message: "Invalid token" });
